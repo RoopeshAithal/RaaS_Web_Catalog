d411e10c2a22620da576edd8468957043e0d8631
RoopeshAithal <RoopeshAithal@gmail.com>
2023-05-05 09:03:53 -0400

    Update RaaSApp.py

--- RaaSApp.py
+++ RaaSApp.py
@@ -4,9 +4,9 @@ import snowflake.connector
 
 app.title('RaaS Web Catalog')
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**app.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+app.text("Hello from Snowflake:")
+app.text(my_data_row)
